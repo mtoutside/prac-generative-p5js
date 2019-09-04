ef9f3b4d6ce7e9ae4d85472c81e19b35f8ae028a
mtoutside <more.humor.or@gmail.com>
2019-09-05 08:42:12 +0900

    add p1111

---
+++ p1111/sketch.js
@@ -0,0 +1,22 @@
+'use strict';
+
+let stepX;
+let stepY;
+
+function setup() {
+	createCanvas(800, 400);
+ noStroke();
+  colorMode(HSB, width, height, 100);
+}
+
+function draw() {
+ stepX = mouseX + 2;
+ stepY = mouseY + 2;
+
+ for(let gridY = 0; gridY < height; gridY += stepY) {
+	for(let gridX = 0; gridX < width; gridX += stepX) {
+	 fill(gridX, height - gridY, 100);
+	 rect(gridX, gridY, stepX, stepY);
+	}
+ }
+}
