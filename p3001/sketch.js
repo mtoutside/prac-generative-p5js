3a4d473ef1786747f2469c9788c9ef4190168b21
mtoutside <more.humor.or@gmail.com>
2019-11-21 02:09:20 +0900

    add p3001

---
+++ p3001/sketch.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let font = 'sans-serif';
+let letter = 'A';
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(255);
+  fill(0);
+
+  textFont(font);
+  textAlign(CENTER, CENTER);
+
+}
+
+function mouseMoved() {
+  clear();
+  textSize((mouseX - width / 2) * 5 + 1);
+  text(letter, width / 2, mouseY);
+}
+
+function mouseDragged() {
+  textSize((mouseX - width / 2) * 5 + 1);
+  text(letter, width / 2, mouseY);
+}
+
+
+function keyReleased() {
+  if (keyCode === CONTROL) saveCanvas(gd.timestamp(), 'png');
+}
+function keyTyped() {
+  letter = key;
+}
