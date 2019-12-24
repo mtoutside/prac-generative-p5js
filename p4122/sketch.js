e24405523053558dd94a94cca71abe2be1e191d4
mtoutside <more.humor.or@gmail.com>
2019-12-24 10:56:50 +0900

    add p4122

--- p4122/sketch.js
+++ p4122/sketch.js
@@ -7,17 +7,18 @@ function preload() {
 }
 function setup() {
 	createCanvas(1024, 780);
-  image(img, 0, 100);
+  image(img, 0, 0);
 }
 
 function draw() {
-  let x1 = floor(random(width));
-  let y1 = 50;
-  let x2 = round(x1 + random(-7, 7));
-  let y2 = round(y1 + random(-5, 5));
+  let x1 = random(width);
+  let y1 = random(height);
 
-  let w = floor(random(10, 40));
-  let h = height - 100;
+  let x2 = round(x1 + random(-10, 10));
+  let y2 = round(y1 + random(-10, 10));
+
+  let w = 150;
+  let h = 50;
 
   set(x2, y2, get(x1, y1, w, h));
 }
@@ -26,6 +27,6 @@ function keyReleased() {
   if (key === 's' || key === 'S') saveCanvas(gd.timestamp(), 'png');
   if(keyCode === DELETE || keyCode === BACKSPACE) {
     clear();
-    image(img, 0, 100);
+    image(img, 0, 0);
   }
 }
