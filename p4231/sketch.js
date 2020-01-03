f7ad31cbbc14bb2b312391fd0a30cbbc32f214e9
mtoutside <more.humor.or@gmail.com>
2020-01-04 02:11:58 +0900

    wip p4231

--- p4231/sketch.js
+++ p4231/sketch.js
@@ -1,40 +1,179 @@
 'use strict';
 
-let cam;
-let intervalTime = 30;
+let videoSrc = '';
+let video;
+let subtitleSrc = '';
+let subtitles;
 
-let secondsSinceStart = 0;
-let startTime = gd.timestamp();
-let counter = 0;
-let doSave = 0;
-let streamReady = false;
+let searchQuery = '\\b(comet)\\b';
 
-function setup() {
-	createCanvas(640, 480);
-  cam = createCapture(VIDEO, () => {
-    streamReady = true;
-  });
-  cam.hide();
-  noStroke();
+let searchResults = [];
+let currentResult;
+
+let fragmentTimer;
+
+let row = 0;
+let col = 0;
+let frameWidth;
+let frameHeight;
+
+let tileMode = true;
+
+let gui;
+
+function preload() {
+  video = createVideo(videoSrc);
+  loadStrings(subtitleSrc, parseSubtitles);
 }
 
-function draw() {
-  if(streamReady) {
-    image(cam ,0, 0, width, width * cam.heignt / cam.width);
+function parseSubtitles(lines) {
+   subtitles = [];
+  let timecodeRegEx = new RegExp(/((\d{2}:){2}\d{2}(,|\.)\d{3})\s-->\s((\d{2}:){2}\d{2}(,|\.)\d{3})/);
+  let subtitleObject;
+  let startTime;
+  let endTime;
+  let dialog;
+  lines.forEach((line, i) => {
+    if(timecodeRegEx.test(line) || i === lines.length) {
+      if(dialog) {
+         subtitles.push(new SubTitleObject(startTime, endTime, dialog));
+      }
 
-    secondsSinceStart = millis() / 1000;
-    let interval = int(secondsSinceStart % intervalTime);
+      if(i < lines.length) {
+        startTime = line.replace(/\s.+$/, '');
+        endTime = line.replace(/^.+\s/, '');
+        dialog = '';
+      }
+    } else {
+      if(startTime && endTime) {
+        dialog += line + ' ';
+      }
+    }
+  });
+  print(subtitles);
+}
 
-    if(interval === 0 && doSave === true) {
-      let saveFileName = startTime + '-' + nf(counter, 5, 0);
-      saveCanvas(saveFileName, 'png');
-      doSave = false;
-      counter++;
-    } else if(interval !== 0) {
-      doSave = true;
+function SubTitleObject(startTime, endTime, dialog) {
+  this.startTimeStamp = startTime;
+  this.endTimeStamp = endTime;
+  this.startTime = getTimeInSeconds(startTime);
+  this.endTime = getTimeInSeconds(endTime);
+  this.dialog = dialog.replace(/\s\d+\s$|<(?:.)*?>/g, '').trim();
+  this.duration = this.endTime - this.startTime;
+}
+
+function getTimeInSeconds(timeString) {
+  let hours = parseInt(timeString.replace(/:.+$/, ''));
+  let minutes = parseInt(timeString.replace(/^\d.+?:|:\d.+$/, ''));
+  let seconds = parseInt(timeString.replace(/^\d.+:|(\,|\.).+$/, ''));
+  let milSeconds = parseInt(timeString.replace(/^.+(\,|\.)/, ''));
+  return (hours * 60 * 60) + (minutes * 60) + seconds + (milSeconds / 1000);
+}
+
+function findSubtiles(searchPattern) {
+  searchPattern = new RegExp(searchPattern, 'i');
+  let results = [];
+  subtitles.forEach(function(subtitle) {
+    if (searchPattern.test(subtitle.dialog)) {
+      results.push(subtitle);
+    };
+  });
+  return results;
+}
+
+function generateMontage() {
+  clearTimeout(fragmentTimer);
+  video.stop();
+  selectAll('.subtitle').forEach((dialogElement) => {
+    dialogElement.remove();
+  });
+  video.elt.style.width = tileMode ? '25%' : '100%';
+  video.position(0, 0);
+  video.show();
+  clear();
+  row = 0;
+  col = 0;
+
+  searchResults = findSubtiles(searchQuery);
+  print(
+    `Found ${searchResults.length} results for search query ${searchQuery}`, searchResults);
+
+  if(searchResults.length) {
+    resizeCanvas(windowWidth, searchResults.length * video.size().height);
+    queryResultMontage(searchResults, 0);
+  }
+}
+
+function queryResultMontage(searchResults, i) {
+  currentResult = searchResults[i];
+  let duration = currentResult.duration;
+
+  video.play();
+  video.time(currentResult.startTime);
+
+  print(currentResult.startTimeStamp, currentResult.dialog);
+  fragmentTimer = setTimeout(() => {
+
+    video.pause();
+
+    if(tileMode) {
+      let framePos = getFramePos();
+      let img = vidoe.get();
+      image(img, framePos.x, framePos.y, frameWidth, frameHeight);
+
+      let dialogElement = createSpan(currentResult.dialog);
+      dialogElement.addClass('subtitle');
+      dialogElement.size(frameWidth, frameHeight);
+      dialogElement.position(framePos.x, framePos.y);
+
+      text(currentResult.endTimeStamp, framePos.x, framePos.y);
+
+      col++;
+      framePos = getFramePos();
+      video.position(framePos.x, framePos.y);
     }
 
-    fill(random(0, 255), random(0, 255), random(0, 255));
-    rect(map(interval, 0, intervalTime, 0, width), 0, 5, 5);
+    if(i < searchResults.length - 1) {
+      queryResultMontage(searchResults, i + 1);
+    } else {
+      video.hide();
+      clearTimeout(fragmentTimer);
+    }
+  }, duration * 1000);
+}
+
+function togglePlayback() {
+  clearTimeout(fragmentTimer);
+  if(video.elt.paused) {
+    video.play();
+  } else {
+    video.pause();
   }
+}
+
+function getFramePos() {
+  frameWidth = video.size().width;
+  frameHeight = video.size().height;
+  let x = frameWidth * col;
+  let y = frameHeight * row;
+  if (x + frameWidth > width) {
+    col = 0;
+    row++;
+    x = frameWidth * col;
+    y = frameHeight * row;
+  }
+  return createVector(x, y);
+}
+
+function setup() {
+	createCanvas(windowWidth, 1000);
+
+  fill(255);
+  stroke(0);
+  textSize(10);
+  textAlign(LEFT, TOP);
+
+  createGUI();
+
+  generateMontage(searchQuery);
 }
