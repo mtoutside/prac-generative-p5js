30b2fd007565fb409e11e400b36fbf4648407668
mtoutside <more.humor.or@gmail.com>
2020-05-27 10:45:03 +0900

    wip m6102 can't run

---
+++ m6102/Spring.js
@@ -0,0 +1,29 @@
+import myp5 from './sketch.js';
+import Node from './Node.js';
+
+export default class Spring {
+  constructor(fromNode, toNode, length, stiffness, damping) {
+    this.fromNode = fromNode;
+    this.toNode = toNode;
+
+    this.length = length || 100;
+    this.stiffness = stiffness || 0.6;
+    this.damping = damping || 0.9;
+  }
+
+  update() {
+    console.log(this.toNode);
+    let diff = p5.Vector.sub(this.toNode, this.fromNode);
+    diff.normalize();
+    diff.mult(this.length);
+    let target = p5.Vector.add(this.fromNode, diff);
+
+    let force = p5.Vector.sub(target, this.toNode);
+    force.mult(0.5);
+    force.mult(this.stiffness);
+    force.mult(1 - this.damping);
+
+    this.toNode.velocity.add(force);
+    this.fromNode.velocity.add(p5.Vector.mult(force, -1));
+  }
+}
